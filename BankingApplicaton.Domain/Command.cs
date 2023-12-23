af5bb084736e0bb94688403c4e5dcd3d6514ced4
aimenovt <meaimenov@gmail.com>
2023-12-23 19:12:35 +0600

    Initial Commit of Rabbit Logic

---
+++ BankingApplicaton.Domain/Command.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankingApplicaton.Domain
+{
+    public abstract class Command : Message
+    {
+        public DateTime TimeStamp { get; protected set; }
+
+        protected Command()
+        {
+            TimeStamp = DateTime.Now;   
+        }
+    }
+}
---
+++ BankingApplicaton.Domain/IEventHandler.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankingApplicaton.Domain
+{
+    public interface IEventHandler<in TEvent> : IEventHandler where TEvent : Event
+    {
+        Task Handle(TEvent @event);
+    }
+
+    public interface IEventHandler
+    {
+
+    }
+}
